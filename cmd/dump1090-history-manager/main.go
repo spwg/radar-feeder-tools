3754405dbe499b00962c48c2ff27c6db38258c8d
Spencer Greene <spencerwgreene@gmail.com>
2024-02-19 22:14:25 -0500

    always merge history files instead of requiring a separate command-line invocation

--- cmd/dump1090-history-manager/main.go
+++ cmd/dump1090-history-manager/main.go
@@ -30,39 +30,40 @@ func run() error {
 		return fmt.Errorf("--data_dir is required")
 	}
 	ctx := context.Background()
-	switch {
-	case *uploadToPostgres:
-		glog.Infof("Reading historical files.")
-		flights, err := history.ReadHistoricalFiles(*dataDir)
+	if err := history.MergeHistoryFiles(*dataDir, *outDir); err != nil {
+		return fmt.Errorf("merge history: %v", err)
+	}
+	if !*uploadToPostgres {
+		return nil
+	}
+	glog.Infof("Reading historical files.")
+	flights, err := history.ReadHistoricalFiles(*dataDir)
+	if err != nil {
+		return err
+	}
+	glog.Infof("Flights %+v", flights)
+	var errUpload error
+	for retry := 0; retry < 3; retry++ {
+		glog.Infof("Connecting to Postgres.")
+		db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
 		if err != nil {
 			return err
 		}
-		glog.Infof("Flights %+v", flights)
-		var errUpload error
-		for retry := 0; retry < 3; retry++ {
-			glog.Infof("Connecting to Postgres.")
-			db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
-			if err != nil {
-				return err
+		defer db.Close()
+		glog.Infof("Uploading to Postgres.")
+		now := time.Now()
+		errUpload = radarstorage.UploadToFlyPostgresInstance(ctx, db, flights)
+		if errUpload != nil {
+			if errors.Is(err, unix.ECONNRESET) {
+				glog.Warningf("Error uploading to postgres: %v", errUpload)
+				continue
 			}
-			defer db.Close()
-			glog.Infof("Uploading to Postgres.")
-			now := time.Now()
-			errUpload = radarstorage.UploadToFlyPostgresInstance(ctx, db, flights)
-			if errUpload != nil {
-				if errors.Is(err, unix.ECONNRESET) {
-					glog.Warningf("Error uploading to postgres: %v", errUpload)
-					continue
-				}
-				return errUpload
-			}
-			glog.Infof("Uploaded in %v.", time.Since(now))
-			return nil
+			return errUpload
 		}
-		return fmt.Errorf("failed to upload flights to the database in 3 tries: latest error: %v", errUpload)
-	default:
-		return history.MergeHistoryFiles(*dataDir, *outDir)
+		glog.Infof("Uploaded in %v.", time.Since(now))
+		return nil
 	}
+	return fmt.Errorf("failed to upload flights to the database in 3 tries: latest error: %v", errUpload)
 }
 
 func main() {
